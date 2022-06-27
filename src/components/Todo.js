5f348028487660062751a9b7b01741cb32f9e616
Duy Linh <doretienshinh2@gmail.com>
2022-06-27 10:18:08 +0700

    Add Firebase to Todojs

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,60 +1,66 @@
-import React, { useState } from 'react';
+import React from 'react';
 
+/* コンポーネント */
 import TodoItem from './TodoItem';
 import Input from './Input';
 import Filter from './Filter';
+import useFbStorage from '../hooks/fbStorage';
 
-import useStorage from '../hooks/storage';
 
-
-const filterItems = ['全て', '未完了', '完了済み'];
 
 function Todo() {
-  const [items, putItems, clearItems] = useStorage();
+  const [items, addItem, updateItem, clearItems] = useFbStorage();
 
-  const [filter, setFilter] = React.useState('全て');
+  const [filter, setFilter] = React.useState('ALL');
 
-  const filterData = React.useMemo(() => {
-    if (filter === '全て') {
-      return items;
-    } else if (filter === '未完了') {
-      return items.filter((item) => !item.done);
-    } else if (filter === '完了済み') {
-      return items.filter((item) => item.done);
-    }
-  }, [filter, items]);
+  const displayItems = items.filter(item => {
+    if (filter === 'ALL') return true;
+    if (filter === 'TODO') return !item.done;
+    if (filter === 'DONE') return item.done;
+  });
 
-  const handleClick = (key) =>
-    putItems(
-      items.map((item) => {
-        if (item.key === key) {
-          return { ...item, done: !item.done };
-        }
-        return item;
-      })
-    );
+  const handleCheck = checked => {
+    updateItem(checked);
+  };
 
-  const addTodo = (todo) => putItems([...items, todo]);
+  const handleAdd = text => {
+    // putItems([...items, { key: getKey(), text, done: false }]);
+    addItem({ text, done: false });
+  };
+
+  const handleFilterChange = value => setFilter(value);
 
   return (
-    <div className="panel">
+    <article class="panel is-danger">
       <div className="panel-heading">
-        ITSS ToDoアプリ
+        <span class="icon-text">
+          <span class="icon">
+            <i class="fas fa-calendar-check"></i>
+          </span>
+          <span> ITSS Todoアプリ</span>
+        </span>
       </div>
-      <Input addTodo={addTodo} />
-      <Filter filterItems={filterItems} filter={filter} setFilter={setFilter} />
-      {filterData.map(item => (
-        <TodoItem handleClick={handleClick} key={item.key} item={item} />
+      <Input onAdd={handleAdd} />
+      <Filter
+        onChange={handleFilterChange}
+        value={filter}
+      />
+      {displayItems.map(item => (
+        <TodoItem 
+          key={item.id}
+          item={item}
+          onCheck={handleCheck}
+        />
       ))}
       <div className="panel-block">
-        {filterData.length} items
+        {displayItems.length} items
       </div>
       <div className="panel-block">
-        <button onClick={clearItems} className="button is-fullwidth">
-          全てのToDoを削除する
+        <button className="button is-light is-fullwidth" onClick={clearItems}>
+          全てのToDoを削除
         </button>
       </div>
-    </div>
+    </article>
   );
 }
 
